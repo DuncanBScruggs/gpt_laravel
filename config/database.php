80cc5a77920b581b55ac16394593e8799a1cee8c
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-12-08 17:06:31 -0500

    heroku

--- config/database.php
+++ config/database.php
@@ -1,28 +1,10 @@
 <?php
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $host = $url["host"] ?? null;
-// $username = $url["user"] ?? null;
-// $password = $url["pass"] ?? null;
-// $database = substr($url["path"], 1);
-// 'mysql' => [
-//             'driver' => 'mysql',
-//             'host' => $host,
-//             'port' => env('DB_PORT', '3306'),
-//             'database' => $database,
-//             'username' => $username,
-//             'password' => $password,
-//             'unix_socket' => env('DB_SOCKET', ''),
-//             'charset' => 'utf8mb4',
-//             'collation' => 'utf8mb4_unicode_ci',
-//             'prefix' => '',
-//             'strict' => true,
-//             'engine' => null,
-//         ]
+
 use Illuminate\Support\Str;
 
 $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
-$host = $dbparts['host'];
+$hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'], '/');
@@ -61,17 +43,32 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
-        // Local Test uncomment for local
-
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $hostname,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -83,23 +80,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
-        // Remote Test uncomment for remote
-
-        // 'mysql' => [
-        //     'driver' => 'mysql',
-        //     'host' => $host,
-        //     'port' => env('DB_PORT', '3306'),
-        //     'database' => $database,
-        //     'username' => $username,
-        //     'password' => $password,
-        //     'unix_socket' => env('DB_SOCKET', ''),
-        //     'charset' => 'utf8mb4',
-        //     'collation' => 'utf8mb4_unicode_ci',
-        //     'prefix' => '',
-        //     'strict' => true,
-        //     'engine' => null,
-        // ],
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
